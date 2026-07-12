e51375fcdcfc81bfc928a653e9303cac2f77d3cc
cas-batista <carrllosma@gmail.com>
2026-07-12 18:56:40 -0300

    fix: corrige nome do campo system_instruction para camelCase (systemInstruction)

--- api/chat.js
+++ api/chat.js
@@ -75,7 +75,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        system_instruction: {
+        systemInstruction: {
           parts: [{ text: systemText }]
         },
         contents: trimmedHistory,
